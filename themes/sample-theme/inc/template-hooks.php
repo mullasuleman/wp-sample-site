7e6eafd341d48b009bd582f5786bd1e1ffa7da4d
Suleman Mulla <mullasuleman12@gmail.com>
2020-01-29 15:03:07 -0500

    added the block editor

--- themes/sample-theme/inc/template-hooks.php
+++ themes/sample-theme/inc/template-hooks.php
@@ -35,3 +35,16 @@ function sample_theme_pingback_header() {
 	}
 }
 add_action( 'wp_head', 'sample_theme_pingback_header' );
+
+// it is best practice to add theme name in the text domain
+// loading block editor JS
+// array specifies the dependencies: wp gives access to the WP, when dom is ready and when we are editing a post
+
+function sample_theme_enqueue_block_editor_assets() {
+    wp_enqueue_script(
+        'block-editor-js',
+		get_template_directory_uri() . '/assets/js/block-editor.js',
+		array( 'wp-blocks', 'wp-dom-ready', 'wp-edit-post' )
+    );
+}
+add_action( 'enqueue_block_editor_assets', 'sample_theme_enqueue_block_editor_assets' );
