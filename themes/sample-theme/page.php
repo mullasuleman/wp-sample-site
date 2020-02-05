6b9963f3b039f1d41a8081f2fb83cf1853a397f3
Suleman Mulla <mullasuleman12@gmail.com>
2020-02-04 19:07:14 -0500

    Adding markup and conditional for the ACF video

--- themes/sample-theme/page.php
+++ themes/sample-theme/page.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying all pages
  *
@@ -15,25 +16,39 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+<div id="primary" class="content-area">
+	<main id="main" class="site-main">
 
 		<?php
-		while ( have_posts() ) :
+		while (have_posts()) :
 			the_post();
 
-			get_template_part( 'template-parts/content', 'page' );
+			// checking if the function exists / False it plugin is deactivated
+			if (function_exists('get_field')) {
+				$featured_video_file = get_field('sample_theme_featured_video_file');
+
+				// checking if the file(url) exists
+				if ($featured_video_file) {
+				?>
+					<video width="320" height="240" controls>
+						<source src="<?php echo $featured_video_file['url']; ?>" type="<?php echo $featured_video_file['mime_type']; ?>">
+						Your browser does not support the video tag.
+					</video>
+				<?php
+				}
+			}
+			get_template_part('template-parts/content', 'page');
 
 			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
+			if (comments_open() || get_comments_number()) :
 				comments_template();
 			endif;
 
 		endwhile; // End of the loop.
 		?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+	</main><!-- #main -->
+</div><!-- #primary -->
 
 <?php
 get_sidebar();
