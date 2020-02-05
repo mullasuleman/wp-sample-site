840f4a10f7b3561e73cf76456c5ef3b229f5abb4
Suleman Mulla <mullasuleman12@gmail.com>
2020-02-04 19:39:03 -0500

    demo of grid container

--- themes/sample-theme/page.php
+++ themes/sample-theme/page.php
@@ -17,7 +17,7 @@ get_header();
 ?>
 
 <div id="primary" class="content-area">
-	<main id="main" class="site-main">
+	<main id="main" class="site-main grid-container">
 
 		<?php
 		while (have_posts()) :
